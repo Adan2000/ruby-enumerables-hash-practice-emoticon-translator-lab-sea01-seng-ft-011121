4052472343625c9af620fcbf1d5e4ea697ce42ba
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-01 06:46:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,20 @@ require 'yaml'
 
 
 def load_library(path)
-  emoji_hash = {}
-  emoji_hash["get_meaning"] = {}
-  emoji_hash["get_emoticon"] = {}
-  emoji = YAML.load_file(path)
-  emoji.each do |word, emojis|
-    emoji_hash["get_meaning"][emojis[1]] = word
-    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
-  end
-  emoji_hash
+
+
+
+
 end
+
+
+
+
+
+
+
+
+
 
 
 
@@ -37,4 +41,22 @@ def get_english_meaning(path, emoticon)
   end
 end
 
+
+
+
+
+
+
+
+
+
+  emoji_hash = {}
+  emoji_hash["get_meaning"] = {}
+  emoji_hash["get_emoticon"] = {}
+  emoji = YAML.load_file(path)
+  emoji.each do |word, emojis|
+    emoji_hash["get_meaning"][emojis[1]] = word
+    emoji_hash["get_emoticon"][emojis[0]] = emojis[1]
+  end
+  emoji_hash
 
