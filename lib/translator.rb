bc1c78d7007d4e42263176c77cd061acb98fb705
Adan Escamilla <adanescamilla580@gmail.com>
2021-01-01 07:19:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,27 +17,6 @@ end
 
 
 
-
-def get_japanese_emoticon(path, emoticon)
-
-
-
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def get_english_meaning(path, emoticon)
 load_library(path).each do |key, value|
   if value[:japanese] == emoticon
